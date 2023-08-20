178422843faa98d263c869562938cc646f29fae3
wx <76164598+nwvh@users.noreply.github.com>
2023-08-20 12:45:24 +0200

    Update drunkrecoil.lua

--- client/drunkrecoil.lua
+++ client/drunkrecoil.lua
@@ -1,46 +1,48 @@
-local drunkAiming = false
-
-function Drunk()
-    local playerPed = PlayerPedId()
-    local playerId = PlayerId()
-
-    local _, weapon = GetCurrentPedWeapon(playerPed,false)
-    if not wx.whitelistedWeapons[weapon] then
-        if IsPlayerFreeAiming(playerId) then
-            if wx.hideCrosshair then
-                HideHudComponentThisFrame( 14 )
+if wx.drunkRecoil then
+    local drunkAiming = false
+    
+    function Drunk()
+        local playerPed = PlayerPedId()
+        local playerId = PlayerId()
+    
+        local _, weapon = GetCurrentPedWeapon(playerPed,false)
+        if not wx.whitelistedWeapons[weapon] then
+            if IsPlayerFreeAiming(playerId) then
+                if wx.hideCrosshair then
+                    HideHudComponentThisFrame( 14 )
+                end
+                DrunkRecoil()
+            elseif IsPedShooting(playerPed) then
+                DrunkRecoil()
+            else
+                if drunkAiming then
+                    drunkAiming = false
+                    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+                end
             end
-            DrunkRecoil()
-        elseif IsPedShooting(playerPed) then
-            DrunkRecoil()
-        else
-            if drunkAiming then
-                drunkAiming = false
-                ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+        end
+    end
+    
+    function DrunkRecoil()
+        if not drunkAiming then
+            drunkAiming = true
+            ShakeGameplayCam("DRUNK_SHAKE", wx.drunkAimingPower)
+        end
+    end
+    
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+            local sleep = true
+            if IsPedArmed(PlayerPedId(), 6) then
+                sleep = false
+                Drunk()
+            end
+            if sleep then
+                Wait(1000)
             end
         end
-    end
+    end)
 end
-
-function DrunkRecoil()
-    if not drunkAiming then
-        drunkAiming = true
-        ShakeGameplayCam("DRUNK_SHAKE", wx.drunkAimingPower)
-    end
-end
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local sleep = true
-        if IsPedArmed(PlayerPedId(), 6) then
-            sleep = false
-            Drunk()
-        end
-        if sleep then
-            Wait(1000)
-        end
-    end
-end)
 
 
